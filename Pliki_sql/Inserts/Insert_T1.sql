c0add27d0eac4509072babbd96339df96517eab4
FeLO <felo.dawid@gmail.com>
2018-11-06 23:56:37 +0100

    SQL: Bulk fix

--- Pliki_sql/Inserts/Insert_T1.sql
+++ Pliki_sql/Inserts/Insert_T1.sql
@@ -1,9 +1,9 @@
 use Policja
 GO
 
-BULK INSERT dbo.Adresy FROM 'Adresy.bulk' WITH (KEEPIDENTITY,FIELDTERMINATOR='|',ROWTERMINATOR = '\n')
-BULK INSERT dbo.Osoby FROM 'Osoby.bulk' WITH (KEEPIDENTITY,FIELDTERMINATOR='|',ROWTERMINATOR = '\n')
-BULK INSERT dbo.Funkcjonariusz FROM 'Funkcjonariusz.bulk' WITH (KEEPIDENTITY,FIELDTERMINATOR='|',ROWTERMINATOR = '\n')
-BULK INSERT dbo.Komisariat FROM 'Komisariat.bulk' WITH (KEEPIDENTITY,FIELDTERMINATOR='|',ROWTERMINATOR = '\n')
-BULK INSERT dbo.Wystawianie_mandatow FROM 'Wystawianie_mandatow.bulk' WITH (KEEPIDENTITY,FIELDTERMINATOR='|',ROWTERMINATOR = '\n')
-BULK INSERT dbo.Przebywanie_w_areszcie FROM 'Przebywanie_w_areszcie.bulk' WITH (KEEPIDENTITY,FIELDTERMINATOR='|',ROWTERMINATOR = '\n')
+BULK INSERT dbo.Adresy FROM 'Adresy.bulk' WITH (FIELDTERMINATOR='|',ROWTERMINATOR = '\n')
+BULK INSERT dbo.Osoby FROM 'Osoby.bulk' WITH (FIELDTERMINATOR='|',ROWTERMINATOR = '\n')
+BULK INSERT dbo.Funkcjonariusz FROM 'Funkcjonariusz.bulk' WITH (FIELDTERMINATOR='|',ROWTERMINATOR = '\n')
+BULK INSERT dbo.Komisariat FROM 'Komisariat.bulk' WITH (FIELDTERMINATOR='|',ROWTERMINATOR = '\n')
+BULK INSERT dbo.Wystawianie_mandatow FROM 'Wystawianie_mandatow.bulk' WITH (FIELDTERMINATOR='|',ROWTERMINATOR = '\n')
+BULK INSERT dbo.Przebywanie_w_areszcie FROM 'Przebywanie_w_areszcie.bulk' WITH (FIELDTERMINATOR='|',ROWTERMINATOR = '\n')
---
+++ Pliki_sql/Inserts/Update_T1_T2.sql
@@ -0,0 +1,18 @@
+UPDATE Osoby SET Nazwisko = X, Numer = X WHERE Id = X;
+UPDATE Osoby SET Nazwisko = X, Numer = X WHERE Id = X; 
+UPDATE Osoby SET Nazwisko = X, Numer = X WHERE Id = X; 
+UPDATE Osoby SET Nazwisko = X, Numer = X WHERE Id = X;
+
+UPDATE Funkcjonariusz SET Stopien=X WHERE Id = X;
+UPDATE Funkcjonariusz SET Stopien=X, Miejsce_przydziału=X WHERE Id = X;  
+UPDATE Funkcjonariusz SET Stopien=X, Miejsce_przydziału=X WHERE Id = X;  
+UPDATE Funkcjonariusz SET Stopien=X, Miejsce_przydziału=X WHERE Id = X;
+
+UPDATE Wystawianie_mandatow SET Powod=X WHERE Id = X;    
+UPDATE Wystawianie_mandatow SET Powod=X WHERE Id = X;  
+UPDATE Wystawianie_mandatow SET Powod=X WHERE Id = X;  
+UPDATE Wystawianie_mandatow SET Powod=X WHERE Id = X;  
+UPDATE Wystawianie_mandatow SET Powod=X WHERE Id = X;  
+
+UPDATE Przebywanie_w_areszcie SET Powod=X, Czas=X WHERE Id = X;
+UPDATE Przebywanie_w_areszcie SET Czas=X WHERE Id = X;
