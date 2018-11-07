1c46890bf63d9dc651dabf95013945a01cea55d9
FeLO <felo.dawid@gmail.com>
2018-11-07 02:16:19 +0100

    Fix

--- Pliki_sql/Inserts/Insert_T2.sql
+++ Pliki_sql/Inserts/Insert_T2.sql
@@ -1,11 +1,9 @@
 use Policja
 GO
 
-
-
-BULK INSERT dbo.Adresy FROM 'Adresy.bulk' WITH (FIELDTERMINATOR='|',ROWTERMINATOR = '\n')
-BULK INSERT dbo.Osoby FROM 'Osoby.bulk' WITH (FIELDTERMINATOR='|',ROWTERMINATOR = '\n')
-BULK INSERT dbo.Funkcjonariusz FROM 'Funkcjonariusz.bulk' WITH (FIELDTERMINATOR='|',ROWTERMINATOR = '\n')
-BULK INSERT dbo.Komisariat FROM 'Komisariat.bulk' WITH (FIELDTERMINATOR='|',ROWTERMINATOR = '\n')
-BULK INSERT dbo.Wystawianie_mandatow FROM 'Wystawianie_mandatow.bulk' WITH (FIELDTERMINATOR='|',ROWTERMINATOR = '\n')
-BULK INSERT dbo.Przebywanie_w_areszcie FROM 'Przebywanie_w_areszcie.bulk' WITH (FIELDTERMINATOR='|',ROWTERMINATOR = '\n')
+BULK INSERT dbo.Adresy FROM 'C:\Users\Felo\Desktop\TMP_HD\Nowy-Generator-HD\t2\Adresy.bulk' WITH (FIELDTERMINATOR='|',ROWTERMINATOR = '\n')
+BULK INSERT dbo.Osoby FROM 'C:\Users\Felo\Desktop\TMP_HD\Nowy-Generator-HD\t2\Osoby.bulk' WITH (FIELDTERMINATOR='|',ROWTERMINATOR = '\n')
+BULK INSERT dbo.Funkcjonariusz FROM 'C:\Users\Felo\Desktop\TMP_HD\Nowy-Generator-HD\t2\Funkcjonariusz.bulk' WITH (FIELDTERMINATOR='|',ROWTERMINATOR = '\n')
+BULK INSERT dbo.Komisariat FROM 'C:\Users\Felo\Desktop\TMP_HD\Nowy-Generator-HD\t2\Komisariat.bulk' WITH (FIELDTERMINATOR='|',ROWTERMINATOR = '\n')
+BULK INSERT dbo.Wystawianie_mandatow FROM 'C:\Users\Felo\Desktop\TMP_HD\Nowy-Generator-HD\t2\WystawienieMandatu.bulk' WITH (FIELDTERMINATOR='|',ROWTERMINATOR = '\n')
+BULK INSERT dbo.Przebywanie_w_areszcie FROM 'C:\Users\Felo\Desktop\TMP_HD\Nowy-Generator-HD\t2\PrzebywanieWAreszcie.bulk' WITH (FIELDTERMINATOR='|',ROWTERMINATOR = '\n')
